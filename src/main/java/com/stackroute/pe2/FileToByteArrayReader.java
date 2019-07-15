cc55950b192aeabe423024761f4f044b39185aa5
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-16 00:17:48 +0530

    Practice Exercise - Question 8 test cases completed.

---
+++ src/main/java/com/stackroute/pe2/FileToByteArrayReader.java
@@ -0,0 +1,22 @@
+package com.stackroute.pe2;
+
+/**
+ * Practice Exercise - Question 8
+ * Write a program to find all files of a folder and select only
+ * given extension fileName and read content of this file using byte array
+ */
+public class FileToByteArrayReader {
+
+    /**
+     * Method to find file based on the file name and extension
+     * given and returns the contents of the file as a byte array.
+     * Throws FileNotFoundException if the file is not found.
+     * @param filename Name of the file to be read
+     * @param extension Extension of the file to be read.
+     * @return Byte array containing the contents of the file
+     */
+    public byte[] readFile(String filename, String extension) {
+        /*Should throw FileNotFoundException if the file is not found*/
+        return null;
+    }
+}
