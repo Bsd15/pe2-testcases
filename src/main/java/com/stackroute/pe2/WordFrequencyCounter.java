b0e39a24dd3804f4f94d59d88169ef8d1f99d4a1
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-16 00:46:23 +0530

    Practice Exercise - Question 9 test cases completed.

---
+++ src/main/java/com/stackroute/pe2/WordFrequencyCounter.java
@@ -0,0 +1,23 @@
+package com.stackroute.pe2;
+
+
+import java.util.Map;
+
+/**
+ * Practice Exercise - Question 9
+ * Write a program to calculate the frequency of the words in a given file
+ */
+public class WordFrequencyCounter {
+
+    /**
+     * The method takes in a file path as String and read the file and
+     * counts the number of times a word appears in the file content. The method
+     * uses a HashMap to store the word frequencies.
+     * Return null if the file is not found.
+     * @param filePath Path of the file to be read
+     * @return A key value pair consisting of word and the number of it's occurrences.
+     */
+    public Map<String, Integer> calculateWordFrequency(String filePath) {
+        return null;
+    }
+}
